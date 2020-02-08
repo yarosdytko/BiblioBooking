b135dce8b9cc2986d71ed90bcd87d9a54a5b2e61
yaros <y.dytko.2017@alumnos.urjc.es>
2020-02-08 09:09:48 +0100

    deleted:    BiblioBookingSTS/.gitignore 	deleted:    BiblioBookingSTS/.mvn/wrapper/MavenWrapperDownloader.java 	deleted:    BiblioBookingSTS/.mvn/wrapper/maven-wrapper.jar 	deleted:    BiblioBookingSTS/.mvn/wrapper/maven-wrapper.properties 	deleted:    BiblioBookingSTS/mvnw 	deleted:    BiblioBookingSTS/mvnw.cmd 	deleted:    BiblioBookingSTS/pom.xml 	deleted:    BiblioBookingSTS/src/main/java/es/code/urjc/BiblioBooking/Alumno.java 	deleted:    BiblioBookingSTS/src/main/java/es/code/urjc/BiblioBooking/AlumnosRepository.java 	deleted:    BiblioBookingSTS/src/main/java/es/code/urjc/BiblioBooking/BiblioBookingApplication.java 	deleted:    BiblioBookingSTS/src/main/java/es/code/urjc/BiblioBooking/indexController.java 	deleted:    BiblioBookingSTS/src/main/resources/application.properties 	deleted:    BiblioBookingSTS/src/main/resources/templates/alumnos_template.html 	deleted:    BiblioBookingSTS/src/test/java/es/code/urjc/BiblioBooking/BiblioBookingApplicationTests.java

--- BiblioBookingSTS/src/main/java/es/code/urjc/BiblioBooking/AlumnosRepository.java
+++
@@ -1,7 +0,0 @@
-package es.code.urjc.BiblioBooking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlumnosRepository extends JpaRepository<Alumno, Long> {
-	
-}
