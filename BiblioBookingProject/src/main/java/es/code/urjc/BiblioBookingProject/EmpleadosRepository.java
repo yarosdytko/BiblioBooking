98b937c0a81946c24c753856b5591962cd446f10
yaros <y.dytko.2017@alumnos.urjc.es>
2020-02-11 17:46:55 +0100

    Creados repositorios para entidades

---
+++ BiblioBookingProject/src/main/java/es/code/urjc/BiblioBookingProject/EmpleadosRepository.java
@@ -0,0 +1,7 @@
+package es.code.urjc.BiblioBookingProject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpleadosRepository extends JpaRepository<Empleado, Long> {
+
+}
---
+++ BiblioBookingProject/src/main/java/es/code/urjc/BiblioBookingProject/SalasRepository.java
@@ -0,0 +1,7 @@
+package es.code.urjc.BiblioBookingProject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalasRepository extends JpaRepository<Sala, Long>  {
+
+}
