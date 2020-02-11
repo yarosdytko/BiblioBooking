2e530b9c2370c9d90663f4660f596017b38fe553
yaros <y.dytko.2017@alumnos.urjc.es>
2020-02-11 17:51:43 +0100

    Creados repositorios para entidades

---
+++ BiblioBookingProject/src/main/java/es/code/urjc/BiblioBookingProject/AdministradoresRepository.java
@@ -0,0 +1,7 @@
+package es.code.urjc.BiblioBookingProject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdministradoresRepository extends JpaRepository<Administrador, Long> {
+
+}
